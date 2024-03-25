f5e5bdf63555152b497184c620009a92ce5bfaaa
YeahOut <pt840072@gmail.com>
2024-03-25 23:08:29 +0900

    2024-03-25 OSSP 개인과제#2

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.print("학번을 입력하세요 : ");
+        String number = sc.nextLine();
+        System.out.println( );
+        System.out.println("<출력>");
+        System.out.println("이름 : " + name);
+        System.out.println("학번 : " + number);
+        }
+    }
